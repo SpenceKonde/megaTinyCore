0381badb50c66fc4841f3c56499ba034a3e30a3f
Spence Konde (aka Dr. Azzy) <spencekonde@gmail.com>
2019-06-16 15:02:20 -0400

    Fix #7
    Dispense with analog pin lookup table alltogether and make
    digitalPinToAnalogInput macro return the analog channel directly.

--- megaavr/variants/csat1616/pins_arduino.h
+++ megaavr/variants/csat1616/pins_arduino.h
@@ -238,7 +238,7 @@ const uint8_t PROGMEM digital_pin_to_timer[] = {
   
   
 };
-
+/*
 const uint8_t PROGMEM analog_pin_to_channel[] = {
   4,
   5,
@@ -252,12 +252,13 @@ const uint8_t PROGMEM analog_pin_to_channel[] = {
   2,
   3
 };
-
+*/
 #endif
 
 extern const uint8_t analog_pin_to_channel[];
 // #define digitalPinToAnalogInput(p)  ((p < NUM_ANALOG_INPUTS) ? pgm_read_byte(analog_pin_to_channel + p) : NOT_A_PIN )
-#define digitalPinToAnalogInput(p) 		(((p) < 6 || (p) == 8 || (p) == 9 || (p) > 13) ? pgm_read_byte(analog_pin_to_channel + p) : NOT_A_PIN)
+//#define digitalPinToAnalogInput(p) 		(((p) < 6 || (p) == 8 || (p) == 9 || (p) > 13) ? pgm_read_byte(analog_pin_to_channel + p) : NOT_A_PIN)
+#define digitalPinToAnalogInput(p)      ((p<6)?(p+4):((p>13)?(p-13):((p==8)?10:(p==9?11:NOT_A_PIN))))
 
 
 // These serial port names are intended to allow libraries and architecture-neutral
