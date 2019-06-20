01810668853620dd7997b3c8a98304a736b5c369
Spence Konde (aka Dr. Azzy) <spencekonde@gmail.com>
2019-06-20 19:45:49 -0400

    Tiny xy4 pinmapping

--- megaavr/variants/txy4/pins_arduino.h
+++ megaavr/variants/txy4/pins_arduino.h
@@ -26,7 +26,7 @@
 #include <avr/pgmspace.h>
 #include "timers.h"
 
-#define NUM_DIGITAL_PINS            11 // 
+#define NUM_DIGITAL_PINS            12 // 
 #define NUM_ANALOG_INPUTS           9
 //#define NUM_RESERVED_PINS           0 // (TOSC1/2, VREF, RESET, DEBUG USART Rx/Tx)
 //#define NUM_INTERNALLY_USED_PINS    0 // (2 x Chip select + 2 x UART + 4 x IO + LED_BUILTIN + 1 unused pin)
@@ -34,17 +34,16 @@
 #define NUM_SPI_PINS                3 // (MISO / MOSI / SCK)
 #define NUM_TOTAL_FREE_PINS         (NUM_DIGITAL_PINS)
 #define NUM_TOTAL_PINS              (NUM_DIGITAL_PINS + NUM_I2C_PINS + NUM_SPI_PINS)
-#define ANALOG_INPUT_OFFSET         0
 
-#define EXTERNAL_NUM_INTERRUPTS     11
+#define EXTERNAL_NUM_INTERRUPTS     12
 
 
-#define digitalPinHasPWM(p)         ((p) == 0 || (p) == 1 || (p) == 7 || (p) == 8 || (p) == 9 || (p) == 16)
+#define digitalPinHasPWM(p)         ((p) == 0 || (p) == 1 || (p) == 5 || (p) == 6 || (p) == 7 || (p) == 10)
 
 #define SPI_MUX		  	(PORTMUX_SPI0_DEFAULT_gc)
-#define PIN_SPI_MISO	(15)
-#define PIN_SPI_SCK		(16)
-#define PIN_SPI_MOSI	(14)
+#define PIN_SPI_MISO	(9)
+#define PIN_SPI_SCK		(10)
+#define PIN_SPI_MOSI	(8)
 #define PIN_SPI_SS		(0)
 
 #define MUX_SPI			(SPI_MUX)
@@ -55,8 +54,8 @@ static const uint8_t MOSI = PIN_SPI_MOSI;
 static const uint8_t MISO = PIN_SPI_MISO;
 static const uint8_t SCK  = PIN_SPI_SCK;
 
-#define PIN_WIRE_SDA        (8)
-#define PIN_WIRE_SCL        (9)
+#define PIN_WIRE_SDA        (6)
+#define PIN_WIRE_SCL        (7)
 
 #define TWI_MUX 		(PORTMUX_TWI0_DEFAULT_gc)
 
@@ -69,24 +68,23 @@ static const uint8_t SCL = PIN_WIRE_SCL;
 #define HWSERIAL0_DRE_VECTOR_NUM (USART0_DRE_vect_num)
 #define HWSERIAL0_RXC_VECTOR 	(USART0_RXC_vect)
 #define HWSERIAL0_MUX 			(PORTMUX_USART0_DEFAULT_gc)
-#define PIN_WIRE_HWSERIAL0_RX 	(6)
-#define PIN_WIRE_HWSERIAL0_TX 	(7)
+#define PIN_WIRE_HWSERIAL0_RX 	(4)
+#define PIN_WIRE_HWSERIAL0_TX 	(5)
 
 #define LED_BUILTIN 4 //FIXME
 
 
 //FIXME These are wrong. They are also wrong on the txy6!
-#define PIN_A0   (0)
-#define PIN_A1   (1)
-#define PIN_A2   (2)
-#define PIN_A3   (3)
-#define PIN_A4   (4)
-#define PIN_A5 	 (5)
-#define PIN_A6	 (8)
-#define PIN_A7   (9)
-#define PIN_A8   (14)
-#define PIN_A9   (15)
-#define PIN_A10  (16)
+#define PIN_A0   (11)
+#define PIN_A1   (8)
+#define PIN_A2   (9)
+#define PIN_A3   (10)
+#define PIN_A4   (0)
+#define PIN_A5 	 (1)
+#define PIN_A6	 (2)
+#define PIN_A7   (3)
+#define PIN_A10  (6)
+#define PIN_A11  (7)
 
 static const uint8_t A0 = PIN_A0;
 static const uint8_t A1 = PIN_A1;
@@ -96,11 +94,10 @@ static const uint8_t A4 = PIN_A4;
 static const uint8_t A5 = PIN_A5;
 static const uint8_t A6 = PIN_A6;
 static const uint8_t A7 = PIN_A7;
-static const uint8_t A8 = PIN_A8;
-static const uint8_t A9 = PIN_A9;
 static const uint8_t A10 = PIN_A10;
+static const uint8_t A11 = PIN_A10;
 
-#define PINS_COUNT		(11u)
+#define PINS_COUNT		(12u)
 
 #ifdef ARDUINO_MAIN
 
@@ -111,12 +108,12 @@ static const uint8_t A10 = PIN_A10;
 // ATtiny1614 / ARDUINO
 //                          _____ 
 //                  VDD   1|*    |20  GND
-// (nSS)  (AIN4) PA4  0~  2|     |19  10~ PA3 (AIN3)(EXTCLK)
+// (nSS)  (AIN4) PA4  0~  2|     |19  10~ PA3 (AIN3)(SCK)(EXTCLK)
 //        (AIN5) PA5  1~  3|     |18  9   PA2 (AIN2)(MISO)
 // (DAC)  (AIN6) PA6  2   4|     |17  8   PA1 (AIN1)(MOSI)
-//        (AIN7) PA7  3   5|     |16      PA0 (nRESET/UPDI)
-// (RXD) (TOSC1) PB3  4   6|     |15  7   PB0 (AIN11)(SCL)
-// (TXD) (TOSC2) PB2  5   7|_____|14  6   PB1 (AIN10)(SDA)
+//        (AIN7) PA7  3   5|     |16  11  PA0 (nRESET/UPDI)
+// (RXD) (TOSC1) PB3  4   6|     |15  7~  PB0 (AIN11)(SCL)
+// (TXD) (TOSC2) PB2  5~  7|_____|14  6~  PB1 (AIN10)(SDA)
 //               
 //
 
@@ -126,17 +123,15 @@ PIN#   DESC         Pin Name  Other/Sp  ADC0      ADC1      PTC       AC0       
 1      A1           PA5       VREFA     AIN5      AIN1      X1/Y1     OUT       AINN0                                                       WO5       TCB0 WO   WOB
 2      A2 or DAC    PA6                 AIN6      AIN2      X2/Y2     AINN0     AINP1     AINP0     OUT
 3      A3           PA7                 AIN7      AIN3      X3/Y3     AINP0     AINP0     AINN0                                                                           LUT1-OUT
-6      RX           PB3       TOSC1                                             OUT                           RxD                           *WO0
-7      TX           PB2       TOSC2 /                                                     OUT                 TxD                           WO2
+4      RX           PB3       TOSC1                                             OUT                           RxD                           *WO0
+5      TX           PB2       TOSC2 /                                                     OUT                 TxD                           WO2
                               EVOUT1                                                      
-8      SDA          PB1                 AIN10               X4/Y4     AINP2                                   XCK                 SDA       WO1
-9      SCL          PB0                 AIN11               X5/Y5               AINP2     AINP1               XDIR                SCL       WO0
-14     MOSI         PA1                 AIN1                                                                  *TxD      MOSI      *SDA                                    LUT0-IN1
-15     MISO         PA2       EVOUT0    AIN2                                                                  *RxD      MISO      *SCL                                    LUT0-IN2
-16     SCK          PA3       EXTCLK    AIN3                                                                  *XCK      SCK                 WO3       TCB1 WO
-NA     VDD          VDD
-NA     GND          GND
-NA?    UPDI         PA0       RESET/    AIN0                                                                                                                              LUT1-IN0
+6      SDA          PB1                 AIN10               X4/Y4     AINP2                                   XCK                 SDA       WO1
+7      SCL          PB0                 AIN11               X5/Y5               AINP2     AINP1               XDIR                SCL       WO0
+8      MOSI         PA1                 AIN1                                                                  *TxD      MOSI      *SDA                                    LUT0-IN1
+9      MISO         PA2       EVOUT0    AIN2                                                                  *RxD      MISO      *SCL                                    LUT0-IN2
+10     SCK          PA3       EXTCLK    AIN3                                                                  *XCK      SCK                 WO3       TCB1 WO
+11     UPDI         PA0       RESET/    AIN0                                                                                                                              LUT1-IN0
                               UPDI        
 	* alternative pin locations			  
 */
@@ -149,12 +144,14 @@ const uint8_t PROGMEM digital_pin_to_port[] = {
 	PA, // 3  PA7
 	PB, // 4  PB3
 	PB, // 5  PB2
-	PB, // 6  PB1
 	// Right side, bottom to top
+	PB, // 6  PB1
 	PB, // 7  PB0
+	// skip PA0 UPDI
 	PA, // 8  PA1
 	PA, // 9  PA2
-	PA  // 10 PA3
+	PA, // 10 PA3
+	PA  // 11 PA0
 };
 
 /* Use this for accessing PINnCTRL register */
@@ -166,12 +163,14 @@ const uint8_t PROGMEM digital_pin_to_bit_position[] = {
 	PIN7_bp, // 3  PA7
 	PIN3_bp, // 4  PB3
 	PIN2_bp, // 5  PB2
-	PIN1_bp, // 6  PB1
 	// Right side, bottom to top
+	PIN1_bp, // 6  PB1
 	PIN0_bp, // 7  PB0
+	//skip PA0 UPDI
 	PIN1_bp, // 8  PA1
 	PIN2_bp, // 9  PA2
-	PIN3_bp  // 10 PA3
+	PIN3_bp, // 10 PA3
+	PIN0_bp  // 11 PA0
 };
 
 /* Use this for accessing PINnCTRL register */
@@ -183,12 +182,14 @@ const uint8_t PROGMEM digital_pin_to_bit_mask[] = {
 	PIN7_bm, // 3  PA7
 	PIN3_bm, // 4  PB3
 	PIN2_bm, // 5  PB2
-	PIN1_bm, // 6  PB1
 	// Right side, bottom to top
+	PIN1_bm, // 6  PB1
 	PIN0_bm, // 7  PB0
+	//skip PA0 UPDI
 	PIN1_bm, // 8  PA1
 	PIN2_bm, // 9  PA2
-	PIN3_bm  // 10 PA3
+	PIN3_bm, // 10 PA3
+	PIN0_bm  // 11 PA0
 };
 
 const uint8_t PROGMEM digital_pin_to_timer[] = {
@@ -199,12 +200,14 @@ const uint8_t PROGMEM digital_pin_to_timer[] = {
 	NOT_ON_TIMER, 	// 3  PA7
 	NOT_ON_TIMER, 	// 4  PB3
 	TIMERA0, 		// 5  PB2
-	TIMERA0, 		// 6  PB1
 	// Right side, bottom to top
+	TIMERA0, 		// 6  PB1
 	TIMERA0, 		// 7  PB0
+	//skip PA0 UPDI
 	NOT_ON_TIMER, 	// 8  PA1
 	TIMERA0, 		// 9  PA2
-	NOT_ON_TIMER  	// 10 PA3
+	NOT_ON_TIMER,  	// 10 PA3
+	NOT_ON_TIMER  	// 11 PA3
   
   
 };
---
+++ megaavr/variants/txy4/variant.c
@@ -0,0 +1,117 @@
+#include <stdbool.h>
+
+#include "pins_arduino.h"
+#include "api/Common.h"
+
+#define FORCE_INLINE __attribute__((always_inline)) inline
+
+void setup_timers() {
+
+	/*  TYPE A TIMER   */
+
+	/* PORTMUX setting for TCA */
+	PORTMUX.CTRLA = PORTMUX_TCA00_DEFAULT_gc;
+	
+	/* Enable Split Mode */
+	TCA0.SPLIT.CTRLD = TCA_SPLIT_SPLITM_bm;
+
+	//Only 1 WGM so no need to specifically set up. 
+
+	/* Period setting, 8-bit register in SPLIT mode */
+	TCA0.SPLIT.LPER	= PWM_TIMER_PERIOD;
+	TCA0.SPLIT.HPER	= PWM_TIMER_PERIOD;
+
+	/* Default duty 50%, will re-assign in analogWrite() */
+	//TODO: replace with for loop to make this smaller;
+	TCA0.SPLIT.LCMP0 = PWM_TIMER_COMPARE;
+	TCA0.SPLIT.LCMP1 = PWM_TIMER_COMPARE;
+	TCA0.SPLIT.LCMP2 = PWM_TIMER_COMPARE;
+	TCA0.SPLIT.HCMP0 = PWM_TIMER_COMPARE;
+	TCA0.SPLIT.HCMP1 = PWM_TIMER_COMPARE;
+	TCA0.SPLIT.HCMP2 = PWM_TIMER_COMPARE;
+
+	/* Use DIV64 prescaler (giving 250kHz clock), enable TCA timer */
+	TCA0.SPLIT.CTRLA = (TCA_SINGLE_CLKSEL_DIV64_gc) | (TCA_SINGLE_ENABLE_bm);
+
+
+	/*	TYPE B TIMERS  */
+
+	/* PORTMUX alternate location needed for TCB0 & 1, TCB2 is default location */
+	//TODO - As part of the timer wrangling involved in #16 and related issues, this stuff needs to be changed. 
+	#if defined(TCBROUTEA)
+	PORTMUX.TCBROUTEA	|= (PORTMUX_TCB0_bm | PORTMUX_TCB1_bm);
+	#endif
+
+	/* Start with TCB0 */
+	TCB_t *timer_B = (TCB_t *)&TCB0;
+
+	/* Timer B Setup loop for TCB[0:2] */
+	#ifdef TCB1
+	do{
+	#endif 
+		/* 8 bit PWM mode, but do not enable output yet, will do in analogWrite() */
+		timer_B->CTRLB = (TCB_CNTMODE_PWM8_gc);
+
+		/* Assign 8-bit period */
+		timer_B->CCMPL = PWM_TIMER_PERIOD;
+
+		/* default duty 50%, set when output enabled */
+		timer_B->CCMPH = PWM_TIMER_COMPARE;
+
+		/* Use TCA clock (250kHz) and enable */
+		/* (sync update commented out, might try to synchronize later */
+		timer_B->CTRLA = (TCB_CLKSEL_CLKTCA_gc)
+						//|(TCB_SYNCUPD_bm)
+						|(TCB_ENABLE_bm);
+
+		/* Increment pointer to next TCB instance */
+		timer_B++;
+
+	/* Stop when pointing to TCB3 */
+	// } while (timer_B < (TCB_t *)&TCB3);
+	
+	// ATtiny only uses up to TCB1
+	/* Stop when pointing to TCB1 */
+	#ifdef TCB1
+	} while (timer_B < (TCB_t *)&TCB1);
+    #endif
+	/* Stuff for synchronizing PWM timers */
+// 	/* Restart TCA to sync TCBs */
+// 	/* should not be needed		*/
+// 	TCA0.SINGLE.CTRLESET = TCA_SINGLE_CMD_RESTART_gc;
+// 	TCA0.SINGLE.CTRLECLR = TCA_SINGLE_CMD_RESTART_gc;
+//
+// 	timer_B = (TCB_t *)&TCB0;
+//
+// 	/* TCB are sync to TCA, remove setting	*/
+// 	for (uint8_t digitial_pin_timer = (TIMERB0 - TIMERB0);
+// 	digitial_pin_timer < (TIMERB3 - TIMERB0);
+// 	digitial_pin_timer++)
+// 	{
+// 		/* disable sync with tca */
+// 		timer_B->CTRLA &= ~ (TCB_SYNCUPD_bm);
+//
+// 		/* Add offset to register	*/
+// 		timer_B++;
+//
+// 	}
+}
+
+FORCE_INLINE bool isDoubleBondedActive(uint8_t pin) {
+	(void)pin;
+
+	/* Check if TWI is operating on double bonded pin (Master Enable is high 
+	in both Master and Slave mode for bus error detection, so this can 
+	indicate an active state for Wire) */
+	//if(((pin == PIN_A4) || (pin == PIN_A5)) && (TWI0.MCTRLA & TWI_ENABLE_bm)) return true;
+
+	/* Special check for SPI_SS double bonded pin -- no action if SPI is active 
+		(Using SPI Enable bit as indicator of SPI activity) */
+	//if((pin == 10) && (SPI0.CTRLA & SPI_ENABLE_bm)) return true;
+
+	return false;
+}
+
+void initVariant() {
+	// NA
+}
