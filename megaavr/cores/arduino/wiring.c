876f25b97627ed7bf5409e6b7dd75a039c9b4bbc
Spence Konde (aka Dr. Azzy) <spencekonde@gmail.com>
2019-06-22 03:32:20 -0400

    Initialize prescalers
    For supported clock speeds of 20, 16, 10, 8, 5, 4, 1 MHz - fix #13

--- megaavr/cores/arduino/wiring.c
+++ megaavr/cores/arduino/wiring.c
@@ -321,11 +321,11 @@ void init()
 	
 /******************************** CLOCK STUFF *********************************/
 
-	/* We assume 5V operating frequency and FUSE.OSCCFG -> 16MHz */
+	
 
  	int64_t cpu_freq;
  	
-	#if (F_CPU == 20000000)
+	#if (F_CPU == 20000000) 
 		cpu_freq = 20000000;
 		
 		/* No division on clock */
@@ -337,41 +337,46 @@ void init()
 		/* No division on clock */
 		_PROTECTED_WRITE(CLKCTRL_MCLKCTRLB, 0x00);
 		
-	#elif (F_CPU == 8000000)
+	#elif (F_CPU == 10000000) //20MHz prescaled by 2
+		cpu_freq = 10000000;
+		
+		/* Clock DIV2 */
+		_PROTECTED_WRITE(CLKCTRL_MCLKCTRLB, (CLKCTRL_PEN_bm | CLKCTRL_PDIV_2X_gc));
+		
+	#elif (F_CPU == 8000000) //16MHz prescaled by 2
 		cpu_freq = 8000000;
 		
 		/* Clock DIV2 */
 		_PROTECTED_WRITE(CLKCTRL_MCLKCTRLB, (CLKCTRL_PEN_bm | CLKCTRL_PDIV_2X_gc));
 		
-	#elif (F_CPU == 4000000)
+	#elif (F_CPU == 5000000) //20MHz prescaled by 4
+		cpu_freq = 5000000;
+		
+		/* Clock DIV4 */
+		_PROTECTED_WRITE(CLKCTRL_MCLKCTRLB, (CLKCTRL_PEN_bm | CLKCTRL_PDIV_4X_gc));
+		
+	#elif (F_CPU == 4000000) //16MHz prescaled by 4
 		cpu_freq = 4000000;
 		
 		/* Clock DIV4 */
 		_PROTECTED_WRITE(CLKCTRL_MCLKCTRLB, (CLKCTRL_PEN_bm | CLKCTRL_PDIV_4X_gc));
 		
-	#elif (F_CPU == 2000000)
-		cpu_freq = 2000000;
+	#elif (F_CPU == 1000000) //16MHz prescaled by 16
+		cpu_freq = 1000000;
 		
 		/* Clock DIV8 */
-		_PROTECTED_WRITE(CLKCTRL_MCLKCTRLB, (CLKCTRL_PEN_bm | CLKCTRL_PDIV_8X_gc));
+		_PROTECTED_WRITE(CLKCTRL_MCLKCTRLB, (CLKCTRL_PEN_bm | CLKCTRL_PDIV_16X_gc));
 	#else
 		
 		#ifndef F_CPU
-			# warning "F_CPU not defined"
-			#define F_CPU 16000000
+			#error "F_CPU not defined"
+		#else 
+	 		#error "F_CPU defined as an unsupported value"
 		#endif
-		
-		# warning "F_CPU defined as an invalid value - may cause undefined behavior"
-		
-		/* Default value is 16MHz */
-		cpu_freq = 16000000;
-				
-		/* No division on clock */
-		_PROTECTED_WRITE(CLKCTRL_MCLKCTRLB, 0x00);
 	#endif
 
 	/* Apply calculated value to F_CPU_CORRECTED */
-	F_CPU_CORRECTED = (uint32_t)cpu_freq;
+	F_CPU_CORRECTED = (uint32_t)cpu_freq; //Spence 6/22/2019: This will always be equal to F_CPU! 
 
 
 /********************************* ADC ****************************************/
