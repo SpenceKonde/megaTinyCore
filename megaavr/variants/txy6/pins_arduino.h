dd930bd3d0a4762e799f3eebae5f4bc2f2c9d8fe
Sander van de Bor <svandeBor@phillipscorp.local>
2019-06-23 22:04:36 -0700

    Revert "Update pins_arduino.h"
    Move analog a# pins before ifdef ARDUINO_MAIN

--- megaavr/variants/txy6/pins_arduino.h
+++ megaavr/variants/txy6/pins_arduino.h
@@ -74,6 +74,32 @@ static const uint8_t SCL = PIN_WIRE_SCL;
 
 #define LED_BUILTIN 4
 
+#define PIN_A0   (17)
+#define PIN_A1   (14)
+#define PIN_A2   (15)
+#define PIN_A3   (16)
+#define PIN_A4   (0)
+#define PIN_A5 	 (1)
+#define PIN_A6	 (2)
+#define PIN_A7   (3)
+#define PIN_A8   (4)
+#define PIN_A9   (5)
+#define PIN_A10  (10)
+#define PIN_A11  (11)
+
+static const uint8_t A0 = PIN_A0;
+static const uint8_t A1 = PIN_A1;
+static const uint8_t A2 = PIN_A2;
+static const uint8_t A3 = PIN_A3;
+static const uint8_t A4 = PIN_A4;
+static const uint8_t A5 = PIN_A5;
+static const uint8_t A6 = PIN_A6;
+static const uint8_t A7 = PIN_A7;
+static const uint8_t A8 = PIN_A8;
+static const uint8_t A9 = PIN_A9;
+static const uint8_t A10 = PIN_A10;
+static const uint8_t A11 = PIN_A11;
+
 #define PINS_COUNT		(18u)
 
 #ifdef ARDUINO_MAIN
@@ -121,32 +147,6 @@ PIN#   DESC         Pin Name  Other/Sp  ADC0      ADC1      PTC       AC0       
                               UPDI        
 	* alternative pin locations			  
 */
-
-#define PIN_A0   (17)
-#define PIN_A1   (14)
-#define PIN_A2   (15)
-#define PIN_A3   (16)
-#define PIN_A4   (0)
-#define PIN_A5 	 (1)
-#define PIN_A6	 (2)
-#define PIN_A7   (3)
-#define PIN_A8   (4)
-#define PIN_A9   (5)
-#define PIN_A10  (10)
-#define PIN_A11  (11)
-
-static const uint8_t A0 = PIN_A0;
-static const uint8_t A1 = PIN_A1;
-static const uint8_t A2 = PIN_A2;
-static const uint8_t A3 = PIN_A3;
-static const uint8_t A4 = PIN_A4;
-static const uint8_t A5 = PIN_A5;
-static const uint8_t A6 = PIN_A6;
-static const uint8_t A7 = PIN_A7;
-static const uint8_t A8 = PIN_A8;
-static const uint8_t A9 = PIN_A9;
-static const uint8_t A10 = PIN_A10;
-static const uint8_t A11 = PIN_A11;
 
 const uint8_t PROGMEM digital_pin_to_port[] = {	
 	// Left side, top to bottom
