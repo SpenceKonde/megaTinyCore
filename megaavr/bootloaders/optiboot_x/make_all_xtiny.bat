1ccb3e74c456cdc5034be8d000326178b4b9e1a2
Spence Konde (aka Dr. Azzy) <spencekonde@gmail.com>
2019-10-14 04:19:59 -0400

    Add bootloader source code
    #52

---
+++ megaavr/bootloaders/optiboot_x/make_all_xtiny.bat
@@ -0,0 +1,16 @@
+call omake tinyxy2
+call omake tinyxy4
+call omake tinyxy6
+call omake tinyxy7
+call omake tinyxy2alt
+call omake tinyxy4alt
+call omake tinyxy6alt
+call omake tinyxy7alt
+call omake tinyxy2rst
+call omake tinyxy6rst
+call omake tinyxy6rst
+call omake tinyxy7rst
+call omake tinyxy2altrst
+call omake tinyxy4altrst
+call omake tinyxy6altrst
+call omake tinyxy7altrst
