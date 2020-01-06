503284c36b9ce10f3c75b8c8d2ea64733d058e48
Spence Konde (aka Dr. Azzy) <spencekonde@gmail.com>
2020-01-06 14:16:36 -0500

    Fix spelling of define preparing for #133

--- megaavr/cores/arduino/api/Common.h
+++ megaavr/cores/arduino/api/Common.h
@@ -109,7 +109,7 @@ void analogWrite(pin_size_t pinNumber, int value);
 
 #ifndef DISABLEMILLIS
 unsigned long millis(void);
-#ifndef MILLIS_USE_TIMER_RTC
+#ifndef MILLIS_USE_TIMERRTC
 unsigned long micros(void);
 #endif
 #endif
