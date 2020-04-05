20f8422fde6259f8790768677a8c8d9b2c6f96e5
Spence Konde (aka Dr. Azzy) <spencekonde@gmail.com>
2020-04-05 17:32:44 -0400

    Added missing datatypes.
    Also fix the indentation to 2 spaces make a tab, as god intended.

--- megaavr/libraries/EEPROM/src/EEPROM.h
+++ megaavr/libraries/EEPROM/src/EEPROM.h
@@ -29,132 +29,131 @@
 #include <avr/io.h>
 
 /***
-    EERef class.
+  EERef class.
 
-    This object references an EEPROM cell.
-    Its purpose is to mimic a typical byte of RAM, however its storage is the EEPROM.
-    This class has an overhead of two bytes, similar to storing a pointer to an EEPROM cell.
+  This object references an EEPROM cell.
+  Its purpose is to mimic a typical byte of RAM, however its storage is the EEPROM.
+  This class has an overhead of two bytes, similar to storing a pointer to an EEPROM cell.
 ***/
 
 #define nvm_read_byte(idx) (*(const uint8_t*)((idx&0xFF)|((idx&0x100)?0x1300:0x1400)))
 
-void nvm_write_byte(idx, dat) {
-    *(uint8_t *)(((idx&0xFF)|((idx&0x100)?0x1300:0x1400)))=dat;
-    uint8_t sreg=SREG;
-    cli();
-    _PROTECTED_WRITE_SPM(NVMCTRL.CTRLA, NVMCTRL_CMD_PAGEERASEWRITE_gc);
-    SREG=sreg;
-    while (NVMCTRL.STATUS & NVMCTRL_EEBUSY_bm);
+void nvm_write_byte(uint16_t idx, uint8_t dat) {
+  *(uint8_t *)(((idx&0xFF)|((idx&0x100)?0x1300:0x1400)))=dat;
+  uint8_t sreg=SREG;
+  cli();
+  _PROTECTED_WRITE_SPM(NVMCTRL.CTRLA, NVMCTRL_CMD_PAGEERASEWRITE_gc);
+  SREG=sreg;
+  while (NVMCTRL.STATUS & NVMCTRL_EEBUSY_bm);
 }
 
+  struct EERef{
 
-struct EERef{
+  EERef( const int index )
+    : index( index )         {}
 
-    EERef( const int index )
-        : index( index )                 {}
+  //Access/read members.
+  uint8_t operator*() const      { return nvm_read_byte( index ); }
+  operator uint8_t() const       { return **this; }
 
-    //Access/read members.
-    uint8_t operator*() const            { return nvm_read_byte( index ); }
-    operator uint8_t() const             { return **this; }
+  //Assignment/write members.
+  EERef &operator=( const EERef &ref ) { return *this = *ref; }
+  EERef &operator=( uint8_t in )     { return nvm_write_byte( (uint8_t*) index, in ), *this;  }
+  EERef &operator +=( uint8_t in )   { return *this = **this + in; }
+  EERef &operator -=( uint8_t in )   { return *this = **this - in; }
+  EERef &operator *=( uint8_t in )   { return *this = **this * in; }
+  EERef &operator /=( uint8_t in )   { return *this = **this / in; }
+  EERef &operator ^=( uint8_t in )   { return *this = **this ^ in; }
+  EERef &operator %=( uint8_t in )   { return *this = **this % in; }
+  EERef &operator &=( uint8_t in )   { return *this = **this & in; }
+  EERef &operator |=( uint8_t in )   { return *this = **this | in; }
+  EERef &operator <<=( uint8_t in )  { return *this = **this << in; }
+  EERef &operator >>=( uint8_t in )  { return *this = **this >> in; }
 
-    //Assignment/write members.
-    EERef &operator=( const EERef &ref ) { return *this = *ref; }
-    EERef &operator=( uint8_t in )       { return nvm_write_byte( (uint8_t*) index, in ), *this;  }
-    EERef &operator +=( uint8_t in )     { return *this = **this + in; }
-    EERef &operator -=( uint8_t in )     { return *this = **this - in; }
-    EERef &operator *=( uint8_t in )     { return *this = **this * in; }
-    EERef &operator /=( uint8_t in )     { return *this = **this / in; }
-    EERef &operator ^=( uint8_t in )     { return *this = **this ^ in; }
-    EERef &operator %=( uint8_t in )     { return *this = **this % in; }
-    EERef &operator &=( uint8_t in )     { return *this = **this & in; }
-    EERef &operator |=( uint8_t in )     { return *this = **this | in; }
-    EERef &operator <<=( uint8_t in )    { return *this = **this << in; }
-    EERef &operator >>=( uint8_t in )    { return *this = **this >> in; }
+  EERef &update( uint8_t in )      { return  in != *this ? *this = in : *this; }
 
-    EERef &update( uint8_t in )          { return  in != *this ? *this = in : *this; }
+  /** Prefix increment/decrement **/
+  EERef& operator++()          { return *this += 1; }
+  EERef& operator--()          { return *this -= 1; }
 
-    /** Prefix increment/decrement **/
-    EERef& operator++()                  { return *this += 1; }
-    EERef& operator--()                  { return *this -= 1; }
+  /** Postfix increment/decrement **/
+  uint8_t operator++ (int){
+    uint8_t ret = **this;
+    return ++(*this), ret;
+  }
 
-    /** Postfix increment/decrement **/
-    uint8_t operator++ (int){
-        uint8_t ret = **this;
-        return ++(*this), ret;
-    }
+  uint8_t operator-- (int){
+    uint8_t ret = **this;
+    return --(*this), ret;
+  }
 
-    uint8_t operator-- (int){
-        uint8_t ret = **this;
-        return --(*this), ret;
-    }
-
-    int index; //Index of current EEPROM cell.
+  int index; //Index of current EEPROM cell.
 };
 
 /***
-    EEPtr class.
+  EEPtr class.
 
-    This object is a bidirectional pointer to EEPROM cells represented by EERef objects.
-    Just like a normal pointer type, this can be dereferenced and repositioned using
-    increment/decrement operators.
+  This object is a bidirectional pointer to EEPROM cells represented by EERef objects.
+  Just like a normal pointer type, this can be dereferenced and repositioned using
+  increment/decrement operators.
 ***/
 
 struct EEPtr{
 
-    EEPtr( const int index )
-        : index( index )                {}
+  EEPtr( const int index )
+    : index( index )        {}
 
-    operator int() const                { return index; }
-    EEPtr &operator=( int in )          { return index = in, *this; }
+  operator int() const        { return index; }
+  EEPtr &operator=( int in )      { return index = in, *this; }
 
-    //Iterator functionality.
-    bool operator!=( const EEPtr &ptr ) { return index != ptr.index; }
-    EERef operator*()                   { return index; }
+  //Iterator functionality.
+  bool operator!=( const EEPtr &ptr ) { return index != ptr.index; }
+  EERef operator*()           { return index; }
 
-    /** Prefix & Postfix increment/decrement **/
-    EEPtr& operator++()                 { return ++index, *this; }
-    EEPtr& operator--()                 { return --index, *this; }
-    EEPtr operator++ (int)              { return index++; }
-    EEPtr operator-- (int)              { return index--; }
+  /** Prefix & Postfix increment/decrement **/
+  EEPtr& operator++()         { return ++index, *this; }
+  EEPtr& operator--()         { return --index, *this; }
+  EEPtr operator++ (int)        { return index++; }
+  EEPtr operator-- (int)        { return index--; }
 
-    int index; //Index of current EEPROM cell.
+  int index; //Index of current EEPROM cell.
 };
 
 /***
-    EEPROMClass class.
+  EEPROMClass class.
 
-    This object represents the entire EEPROM space.
-    It wraps the functionality of EEPtr and EERef into a basic interface.
-    This class is also 100% backwards compatible with earlier Arduino core releases.
+  This object represents the entire EEPROM space.
+  It wraps the functionality of EEPtr and EERef into a basic interface.
+  This class is also 100% backwards compatible with earlier Arduino core releases.
 ***/
 
 struct EEPROMClass{
 
-    //Basic user access methods.
-    EERef operator[]( const int idx )    { return idx; }
-    uint8_t read( int idx )              { return EERef( idx ); }
-    void write( int idx, uint8_t val )   { (EERef( idx )) = val; }
-    void update( int idx, uint8_t val )  { EERef( idx ).update( val ); }
+  //Basic user access methods.
+  EERef operator[]( const int idx )  { return idx; }
+  uint8_t read( int idx )        { return EERef( idx ); }
+  void write( int idx, uint8_t val )   { (EERef( idx )) = val; }
+  void update( int idx, uint8_t val )  { EERef( idx ).update( val ); }
 
-    //STL and C++11 iteration capability.
-    EEPtr begin()                        { return 0x00; }
-    EEPtr end()                          { return length(); } //Standards requires this to be the item after the last valid entry. The returned pointer is invalid.
-    uint16_t length()                    { return EEPROM_SIZE; }
+  //STL and C++11 iteration capability.
+  EEPtr begin()            { return 0x00; }
+  EEPtr end()              { return length(); } //Standards requires this to be the item after the last valid entry. The returned pointer is invalid.
+  uint16_t length()          { return EEPROM_SIZE; }
 
-    //Functionality to 'get' and 'put' objects to and from EEPROM.
-    template< typename T > T &get( int idx, T &t ){
-        EEPtr e = idx;
-        uint8_t *ptr = (uint8_t*) &t;
-        for( int count = sizeof(T) ; count ; --count, ++e )  *ptr++ = *e;
-        return t;
-    }
+  //Functionality to 'get' and 'put' objects to and from EEPROM.
+  template< typename T > T &get( int idx, T &t ){
+    EEPtr e = idx;
+    uint8_t *ptr = (uint8_t*) &t;
+    for( int count = sizeof(T) ; count ; --count, ++e )  *ptr++ = *e;
+    return t;
+  }
 
-    template< typename T > const T &put( int idx, const T &t ){
-        EEPtr e = idx;
-        const uint8_t *ptr = (const uint8_t*) &t;
-        for( int count = sizeof(T) ; count ; --count, ++e )  (*e).update( *ptr++ );
-        return t;
-    }
+  template< typename T > const T &put( int idx, const T &t ){
+    EEPtr e = idx;
+    const uint8_t *ptr = (const uint8_t*) &t;
+    for( int count = sizeof(T) ; count ; --count, ++e )  (*e).update( *ptr++ );
+    return t;
+  }
 };
 
 static EEPROMClass EEPROM;
