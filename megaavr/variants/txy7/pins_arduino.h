7283685f2c500502a61a268e2c93fe37ce37df76
Spence Konde <spencekonde@gmail.com>
2021-02-12 11:30:38 -0500

    Fuck C

--- megaavr/variants/txy7/pins_arduino.h
+++ megaavr/variants/txy7/pins_arduino.h
@@ -122,54 +122,54 @@
 
 #define PINS_COUNT        (22u)
 
-static const uint8_t PIN_PA4 = 0;
-static const uint8_t PIN_PA5 = 1;
-static const uint8_t PIN_PA6 = 2;
-static const uint8_t PIN_PA7 = 3;
-static const uint8_t PIN_PB7 = 4;
-static const uint8_t PIN_PB6 = 5;
-static const uint8_t PIN_PB5 = 6;
-static const uint8_t PIN_PB4 = 7;
-static const uint8_t PIN_PB3 = 8;
-static const uint8_t PIN_PB2 = 9;
-static const uint8_t PIN_PB1 = 10;
-static const uint8_t PIN_PB0 = 11;
-static const uint8_t PIN_PC0 = 12;
-static const uint8_t PIN_PC1 = 13;
-static const uint8_t PIN_PC2 = 14;
-static const uint8_t PIN_PC3 = 15;
-static const uint8_t PIN_PC4 = 16;
-static const uint8_t PIN_PC5 = 17;
-static const uint8_t PIN_PA0 = 21;
-static const uint8_t PIN_PA1 = 18;
-static const uint8_t PIN_PA2 = 19;
-static const uint8_t PIN_PA3 = 20;
+#define PIN_PA4 0
+#define PIN_PA5 1
+#define PIN_PA6 2
+#define PIN_PA7 3
+#define PIN_PB7 4
+#define PIN_PB6 5
+#define PIN_PB5 6
+#define PIN_PB4 7
+#define PIN_PB3 8
+#define PIN_PB2 9
+#define PIN_PB1 10
+#define PIN_PB0 11
+#define PIN_PC0 12
+#define PIN_PC1 13
+#define PIN_PC2 14
+#define PIN_PC3 15
+#define PIN_PC4 16
+#define PIN_PC5 17
+#define PIN_PA0 21
+#define PIN_PA1 18
+#define PIN_PA2 19
+#define PIN_PA3 20
 
-#define PIN_A0   PIN_PA0
-#define PIN_A1   PIN_PA1
-#define PIN_A2   PIN_PA2
-#define PIN_A3   PIN_PA3
-#define PIN_A4   PIN_PA4
-#define PIN_A5   PIN_PA5
-#define PIN_A6   PIN_PA6
-#define PIN_A7   PIN_PA7
-#define PIN_A8   PIN_PB5
-#define PIN_A9   PIN_PB4
-#define PIN_A10  PIN_PB1
-#define PIN_A11  PIN_PB0
+#define PIN_A0     A0
+#define PIN_A1     A1
+#define PIN_A2     A2
+#define PIN_A3     A3
+#define PIN_A4     A4
+#define PIN_A5     A5
+#define PIN_A6     A6
+#define PIN_A7     A7
+#define PIN_A8     A8
+#define PIN_A9     A9
+#define PIN_A10    A10
+#define PIN_A11    A11
 
-static const uint8_t A0  = PIN_A0;
-static const uint8_t A1  = PIN_A1;
-static const uint8_t A2  = PIN_A2;
-static const uint8_t A3  = PIN_A3;
-static const uint8_t A4  = PIN_A4;
-static const uint8_t A5  = PIN_A5;
-static const uint8_t A6  = PIN_A6;
-static const uint8_t A7  = PIN_A7;
-static const uint8_t A8  = PIN_A8;
-static const uint8_t A9  = PIN_A9;
-static const uint8_t A10 = PIN_A10;
-static const uint8_t A11 = PIN_A11;
+static const uint8_t A0  = PIN_PA0;
+static const uint8_t A1  = PIN_PA1;
+static const uint8_t A2  = PIN_PA2;
+static const uint8_t A3  = PIN_PA3;
+static const uint8_t A4  = PIN_PA4;
+static const uint8_t A5  = PIN_PA5;
+static const uint8_t A6  = PIN_PA6;
+static const uint8_t A7  = PIN_PA7;
+static const uint8_t A8  = PIN_PB5;
+static const uint8_t A9  = PIN_PB4;
+static const uint8_t A10 = PIN_PB1;
+static const uint8_t A11 = PIN_PB0;
 
 
 
