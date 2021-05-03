fad3132437b5b748616c3a045ee86477bd03b032
Spence Konde <spencekonde@gmail.com>
2021-05-02 21:20:10 -0400

    Update optiboot, other assorted trivial stuff

--- megaavr/bootloaders/optiboot_x/make_all_xtiny.bat
+++ megaavr/bootloaders/optiboot_x/make_all_xtiny.bat
@@ -1,15 +1,25 @@
 call omake tinyxy2
 call omake tinyxyz
-call omake tinyxy2alt
 call omake tinyxyzalt
+call omake tinyx2z
+call omake tinyx2zalt
+call omake tinyx2zu1alt
+
 call omake tinyxy2rst
 call omake tinyxyzrst
-call omake tinyxy2altrst
 call omake tinyxyzaltrst
+call omake tinyx2zrst
+call omake tinyx2zaltrst
+call omake tinyx2zu1altrst
+
 call omake tinyxy21sec
 call omake tinyxyz1sec
-call omake tinyxy2alt1sec
 call omake tinyxyzalt1sec
+call omake tinyx2z1sec
+call omake tinyx2zalt1sec
+call omake tinyx2zu1alt1sec
+
 call omake xplained817
 call omake xplained416
 call omake curiosity1607
+call omake curiosity1627
