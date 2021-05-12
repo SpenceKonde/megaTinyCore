2e9460d7df812a9d01f941a8f6d1d8ad425b3091
Spence Konde <spencekonde@gmail.com>
2021-05-12 19:33:58 -0400

    silly mistake

--- megaavr/libraries/Event/examples/Simple_event/Simple_event.ino
+++ megaavr/libraries/Event/examples/Simple_event/Simple_event.ino
@@ -4,12 +4,13 @@
 | Simple_Event.ino                                                      |
 |                                                                       |
 | A library for interfacing with the megaAVR event system.              |
-| Developed in 2021 by MCUdude                                          |
+| Developed in 2021 by MCUdude and adapted to tinyAVR by Spence Konde   |
 | https://github.com/MCUdude/                                           |
 |                                                                       |
-| In this example we use pin PE0 as an event generator, and pin PE2 and |
-| PF2 as event users. In practive, pin PE2 and PF2 will follow          |
-| PE0's state.                                                          |
+| In this example we use pin PA1 as an event generator, and pin PA2 and |
+| as event users. In practive, pin PA2 will follow PA1's state          |
+| This trivial example is works without modification on both the 0/1-   |
+| series and the 2=series. with no #ifdefs. That is rare.               |
 |                                                                       |
 | See Microchip's application note AN2451 for more information.         |
 |***********************************************************************/
@@ -17,16 +18,16 @@
 #include <Event.h>
 
 void setup() {
-  // Since pin PE0 is only available on event generator channel 4 and 5, we use Event4 as our object
-  // Note that we use gen4:: to refer to functionality unique to event channel 4
-  Event4.set_generator(gen4::pin_pe0); // Set pin PE0 as event generator
+
+  // For
+  // Note that we use gen2:: to refer to functionality unique to event channel 2
+  Event2.set_generator(gen2::pin_pa1); // Set pin PA1 as event generator
 
   // For more information about EVOUT, see the PORTMUX section in the datasheet
-  Event4.set_user(user::evoute_pin_pe2); // Set EVOUTE as event user
-  Event4.set_user(user::evoutf_pin_pf2); // Set EVOUTF as event user
+  Event2.set_user(user::evouta_pin_pa2); // Set EVOUTA as event user
 
   // Start the event channel
-  Event4.start();
+  Event2.start();
 }
 
 void loop() {
