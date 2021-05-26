8e759ae2ed6724a5ee8f8335692a8abf6da1ed1b
Spence Konde <spencekonde@gmail.com>
2021-05-26 15:06:23 -0400

    dead code removal

--- megaavr/cores/megatinycore/main.cpp
+++ megaavr/cores/megatinycore/main.cpp
@@ -1,53 +1,30 @@
-/*
-  main.cpp - Main loop for Arduino sketches
-  Copyright (c) 2005-2013 Arduino Team.  All right reserved.
-
-  This library is free software; you can redistribute it and/or
-  modify it under the terms of the GNU Lesser General Public
-  License as published by the Free Software Foundation; either
-  version 2.1 of the License, or (at your option) any later version.
-
-  This library is distributed in the hope that it will be useful,
-  but WITHOUT ANY WARRANTY; without even the implied warranty of
-  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
-  Lesser General Public License for more details.
-
-  You should have received a copy of the GNU Lesser General Public
-  License along with this library; if not, write to the Free Software
-  Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
-*/
+/* main.cpp - Main loop for Arduino sketches
+   Part of megaTinyCore - github.com/SpenceKonde/megaTinyCore
+   Copyright (c) 2018~2021 Spence Konde, (c) 2005-2013 Arduino
+   Free Software - LGPL 2.1, please see LICENCE.md for details */
 
 #include <Arduino.h>
 
-// Declared weak in Arduino.h to allow user redefinitions.
-int atexit(void ( * /*func*/)()) {
-  return 0;
-}
-
-// Weak empty variant initialization function.
-// May be redefined by variant files (we don't
-// do that) or other libraries. FreeRTOS is one
-// such library.
+/* Required by some libraries to compile successfully. Even though it's nonsense in Arduino. */
+int atexit(void ( * /*func*/)()) { return 0; }
 
 void initVariant() __attribute__((weak));
+/* Weak empty variant initialization function. The purpose is unclear. It sounds like it was intended
+ * initialize the variant, and specific variants would have their own implementation. But in practice
+ * it seems to be instead used as an initialization callback that libraries can use to run code before
+ * setup, like FreeRTOS - it would have been nice if the official API included such a callback. */
 void initVariant() { }
 
-// these parts don't have USB - but hey, VUSB
-// has been ported to AVRxt, so you never know!
-
-void setupUSB() __attribute__((weak));
-void setupUSB() { }
-
 int main()  __attribute__((weak));
+/* The main function - call initialization functions (in wiring.c) then setup, and finally loop repeatedly.
+ * If serial event is enabled (which should be unusual, as it is no longer a menu option even, that gets
+ * checked for after each call to loop.
+ * In the past there was a USB-related function here, that is removed, as work will be needed in any event
+ * at the core level if VUSB-based "stuff" arrives that needs support at this level */
+
 int main() {
   init(); //Interrupts are turned on just prior to init() returning.
-
   initVariant();
-
-#if defined(USBCON)
-  USBDevice.attach();
-#endif
-
   setup();
   for (;;) {
     loop();
@@ -55,5 +32,4 @@ int main() {
       if (serialEventRun) serialEventRun();
     #endif
   }
-  return 0;
 }
