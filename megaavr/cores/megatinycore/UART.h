d6dd61637e463f7ec2c04e39ec23ec73bdcbe260
Spence Konde <spencekonde@gmail.com>
2021-11-29 12:50:20 -0500

    Fix name of wiremode menu -

--- megaavr/cores/megatinycore/UART.h
+++ megaavr/cores/megatinycore/UART.h
@@ -56,7 +56,7 @@
  * |  128k |       -  |       -  |       -  |   -  |  16384 |   -  |
  * This ratio is remarkably consistent. No AVR part was ever made with
  * less than 8:1 flash:ram, nor more than 16:1, since first ATmegas!
- * The sole exception? The ATmega2560/2561 has only 4k flash.
+ * The sole exception? The ATmega2560/2561 has only 4k RAM.
  * (to be fair, you are allowed to use external RAM - also I think
  * a unique feature)
  */
@@ -162,18 +162,34 @@
 #define SERIAL_7O2 (USART_CMODE_ASYNCHRONOUS_gc | USART_CHSIZE_7BIT_gc | USART_PMODE_ODD_gc | USART_SBMODE_2BIT_gc)
 #define SERIAL_8O2 (USART_CMODE_ASYNCHRONOUS_gc | USART_CHSIZE_8BIT_gc | USART_PMODE_ODD_gc | USART_SBMODE_2BIT_gc)
 
+#if defined(USART_RS4850_bm) && !defined(USART_RS485_bm)
+  #define USART_RS485_bm USART_RS4850_bm
+#endif
+
+
 #define SERIAL_PIN_SETS 2
+// TX, RX, XCK, XDIR
+const uint8_t _usart_pins[][4] = {
+  #if defined(__ATtinyxy2__)
+    {PIN_PA6, PIN_PA7, PIN_PA3, PIN_PA0},
+    {PIN_PA1, PIN_PA2, NOT_A_PIN, NOT_A_PIN},
+  #else
+    {PIN_PB2, PIN_PB3, PIN_PB1, PIN_PB0},
+    {PIN_PA1, PIN_PA2, PIN_PA3, PIN_PA4},
+    #if MEGATINYCORE_SERIES == 2
+      #if !defined(__ATtinyxy4__)
+        {PIN_PC2, PIN_PC1, PIN_PC0, PIN_PC3},
+      #else
+        {NOT_A_PIN, NOT_A_PIN, NOT_A_PIN, NOT_A_PIN}
+      #endif
+    #endif
+  #endif
+}
 
 class UartClass : public HardwareSerial {
   protected:
     volatile USART_t *const _hwserial_module;
-
-    struct UartPinSet {
-      uint8_t const rx_pin;
-      uint8_t const tx_pin;
-      uint8_t const mux;
-    } _hw_set[SERIAL_PIN_SETS];
-
+    const uint8_t _module_number;
     uint8_t _pin_set;
 
     // Has any byte been written to the UART since begin()
@@ -184,8 +200,6 @@ class UartClass : public HardwareSerial {
     volatile tx_buffer_index_t _tx_buffer_head;
     volatile tx_buffer_index_t _tx_buffer_tail;
 
-    volatile uint8_t _hwserial_dre_interrupt_vect_num;
-
     // Don't put any members after these buffers, since only the first
     // 32 bytes of this struct can be accessed quickly using the ldd
     // instruction.
@@ -194,80 +208,59 @@ class UartClass : public HardwareSerial {
 
   public:
     inline UartClass(volatile USART_t *hwserial_module, uint8_t hwserial_rx_pin, uint8_t hwserial_tx_pin, uint8_t hwserial_rx_pin_swap, uint8_t hwserial_tx_pin_swap, uint8_t dre_vect_num, uint8_t uart_mux, uint8_t uart_mux_swap);
-    bool pins(uint8_t tx, uint8_t rx);
-    bool swap(uint8_t state = 1);
-    void begin(unsigned long baud) {
-      begin(baud, SERIAL_8N1);
-    }
-    void begin(unsigned long, uint16_t);
-    void end();
+    bool                    pins(uint8_t tx, uint8_t rx);
+    bool                    swap(uint8_t mux_level = 1);
+    void                   begin(uint32_t baud) {begin(baud, SERIAL_8N1);}
+    void                   begin(uint32_t baud, uint16_t options);
+    void                     end();
+    void                printHex(const     uint8_t              b);
+    void                printHex(const    uint16_t  w, bool s = 0);
+    void                printHex(const    uint32_t  l, bool s = 0);
+    void                printHex(const      int8_t  b)              {printHex((uint8_t)    b);            }
+    void                printHex(const        char  b)              {printHex((uint8_t)    b);            }
+    void              printHexln(const      int8_t  b)              {printHex((uint8_t)    b); println(); }
+    void              printHexln(const        char  b)              {printHex((uint8_t)    b); println(); }
+    void              printHexln(const     uint8_t  b)              {printHex(             b); println(); }
+    void              printHexln(const    uint16_t  w, bool s = 0)  {printHex(          w, s); println(); }
+    void              printHexln(const    uint32_t  l, bool s = 0)  {printHex(          l, s); println(); }
+    void              printHexln(const     int16_t  w, bool s = 0)  {printHex((uint16_t)w, s); println(); }
+    void              printHexln(const     int32_t  l, bool s = 0)  {printHex((uint16_t)l, s); println(); }
+    uint8_t *           printHex(          uint8_t* p, uint8_t len, char sep = 0            );
+    uint16_t *          printHex(         uint16_t* p, uint8_t len, char sep = 0, bool s = 0);
+    volatile uint8_t *  printHex(volatile  uint8_t* p, uint8_t len, char sep = 0            );
+    volatile uint16_t * printHex(volatile uint16_t* p, uint8_t len, char sep = 0, bool s = 0);
 
-    void printHex(const uint8_t b);
-    void printHex(const uint16_t w, bool swaporder = 0);
-    void printHex(const uint32_t l, bool swaporder = 0);
-
-    void printHex(const int8_t b) { printHex((uint8_t)b); }
-    void printHex(const int16_t w, bool swaporder = 0) { printHex((uint16_t)w, swaporder); }
-    void printHex(const int32_t l, bool swaporder = 0) { printHex((uint32_t)l, swaporder); }
-
-    uint8_t * printHex(uint8_t* p,uint8_t len, char sep= 0 );
-    uint16_t * printHex(uint16_t* p, uint8_t len, char sep = 0, bool swaporder = 0);
-
-    void printHexln(const uint8_t b) { printHex(b); println(); }
-    void printHexln(const uint16_t w, bool swaporder = 0) { printHex(w,swaporder); println(); }
-    void printHexln(const uint32_t l, bool swaporder = 0) { printHex(l,swaporder); println(); }
-
-    void printHexln(const int8_t b) { printHex(b); println(); }
-    void printHexln(const int16_t w, bool swaporder = 0) { printHex(w,swaporder); println(); }
-    void printHexln(const int32_t l, bool swaporder = 0) { printHex(l,swaporder); println(); }
-
-    uint8_t * printHexln(uint8_t* p,uint8_t len, char sep= 0 ) {p = printHex(p,len,sep); println(); return p;}
-    uint16_t * printHexln(uint16_t* p, uint8_t len, char sep = 0, bool swaporder = 0) {p = printHex(p,len,sep,swaporder); println(); return p;}
-
-    virtual int available(void);
-    virtual int peek(void);
-    virtual int read(void);
     virtual int availableForWrite(void);
-    virtual void flush(void);
-    virtual size_t write(uint8_t);
-    inline size_t write(unsigned long n) {
-      return write((uint8_t)n);
-    }
-    inline size_t write(long n) {
-      return write((uint8_t)n);
-    }
-    inline size_t write(unsigned int n) {
-      return write((uint8_t)n);
-    }
-    inline size_t write(int n) {
-      return write((uint8_t)n);
-    }
+    virtual int available(void);
+    virtual      int peek(void);
+    virtual      int read(void);
+    virtual    void flush(void);
+    virtual  size_t write(uint8_t ch);
+    inline   size_t write(unsigned long n)  {return write((uint8_t)n);}
+    inline   size_t write(long n)           {return write((uint8_t)n);}
+    inline   size_t write(unsigned int n)   {return write((uint8_t)n);}
+    inline   size_t write(int n)            {return write((uint8_t)n);}
     using Print::write; // pull in write(str) and write(buf, size) from Print
     explicit operator bool() {
       return true;
     }
 
     // Interrupt handlers - Not intended to be called externally
-    inline void _rx_complete_irq(void);
-    void _tx_data_empty_irq(void);
+
+    // Interrupt handlers - Not intended to be called externally
+    static void _rx_complete_irq(UartClass& uartClass);
+    static void _tx_data_empty_irq(UartClass& uartClass);
 
   private:
     void _poll_tx_data_empty(void);
+    static void        _set_pins(uint8_t* pinInfo, uint8_t port_num, uint8_t mux_setting, uint8_t enmask);
+    static void         _mux_set(uint8_t* pinInfo, uint8_t port_num, uint8_t mux_code);
+    static uint8_t _pins_to_swap(uint8_t* pinInfo, uint8_t port_num, uint8_t tx_pin, uint8_t rx_pin);
 };
 
-#if defined(HWSERIAL0)
-  extern UartClass Serial;
-  #define HAVE_HWSERIAL0
+#if defined(USART0)
+  extern UartClass Serial0;
 #endif
-#if defined(HWSERIAL1)
+#if defined(USART1)
   extern UartClass Serial1;
-  #define HAVE_HWSERIAL1
-#endif
-#if defined(HWSERIAL2)
-  extern UartClass Serial2;
-  #define HAVE_HWSERIAL2
-#endif
-#if defined(HWSERIAL3)
-  extern UartClass Serial3;
-  #define HAVE_HWSERIAL3
 #endif
