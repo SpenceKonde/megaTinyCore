0c3eed79bd1e28fb73b7b9e377f59d14b2634a08
Spence Konde <spencekonde@gmail.com>
2021-12-13 07:53:29 -0500

    Formatting, comments, fix 202/402 test
    I don't think the 202/402 test worked.
    
    It should either use the megaTinyCore test __AVR_ATtinyx02__ or, seeing as there are only two parts, we might as well just use the one from the Microchiop supplied headers!

--- megaavr/libraries/Wire/src/twi.h
+++ megaavr/libraries/Wire/src/twi.h
@@ -1,5 +1,6 @@
 /*
   Copyright (c) 2021 MX682X
+  With comparatively trivial modifications by Spence Konde.
 
 Permission is hereby granted, free of charge, to any person obtaining a copy
 of this software and associated documentation files (the "Software"), to deal
@@ -52,7 +53,6 @@ SOFTWARE.
   // If pins for Wire1 are not defined, but TWI_USING_WIRE1 was defined in the boards.txt menu, throw an error. Used for 28-pin DA/DB parts
   #error "This part only provides a single Wire interface."
 #endif
-
 #if ((defined(TWI0_DUALCTRL) && !defined(TWI_USING_WIRE1)) || (defined(TWI1_DUALCTRL) && defined(TWI_USING_WIRE1)))
   /* Instead of requiring changes to the library to switch between DxCore and megaTinyCore, we can check
    * if the part supports dual mode. Goal is that the identical library can be used on both, so updates
@@ -60,7 +60,7 @@ SOFTWARE.
   #define TWI_DUALCTRL   // This identifies if the device supports dual mode, where slave pins are different from the master pins
 #endif
 
-#if defined(ARDUINO_AVR_ATtiny202) || defined(ARDUINO_AVR_ATtiny402)
+#if defined(__AVR_ATtiny202__) || defined(__AVR_ATtiny202__)
   #if defined(TWI_MANDS)  // 202 and 402 do not support independent master and slave.
     // #undef TWI_MANDS
     #error "Master + Slave mode is not supported on the 202 or 402."
@@ -68,26 +68,41 @@ SOFTWARE.
     // so that they know what's wrong instead of silently disobeying
   #endif
 #endif
-
-
+/* Reasoning behind buffer lengths of 16 bytes for stuff with hardly
+ * any RAM, 130 for stuff with tons and 32 for everything else.
+ *
+ * Parts with 128b of RAM wince at pair of 16b buffers (25%)
+ *
+ * In the past we configured this with more granularity but it
+ * was pointed out that because of the official cores all using
+ * 32 byte buffers, many libraries implicitly depend >= 32 byte
+ * buffers and will misbehave if the buffer used is smaller.
+ * So even where it's painfully large portion of ram, we use
+ * that.Similarly, most libraries will not make use of a larger one
+ * Thus, we don't enlarge the buffer until we finally get to
+ * the modern AVRs with >= 4k of RAM, where it doesn't really
+ * matter anymore. At that point, enlarging the buffer to 130b
+ * allows for a 128b page write to the popular 24-series EEPROMs
+ * Since the modern AVRs are lighter on EEPROM than classic ones
+ * it is more likely that external storage would be needed.
+ * SD cards are a poor choice for byte oriented data storage, in
+ * addition to being astonishingly unrelible, and byte oriented
+ * data storage is more useful for typical applications of AVRs
+ * than file-oriented storage.
+ */
 #ifndef BUFFER_LENGTH
-  #if (RAMSIZE < 256)          /* Parts with 128b of RAM wince at pair of 16b buffers         */
-    #define BUFFER_LENGTH 16   /* 2k tinyAVRs: 128b -  25% of available RAM                   */
-  #elif (RAMSIZE < 512)        /* In the past we configured this with more granularity but it */
-    #define BUFFER_LENGTH 32   /* was pointed out that because of the official cores all using*/
-  #elif (RAMSIZE < 2048)       /* 32 byte buffers, many libraries implicitly depend >= 32 byte*/
-    #define BUFFER_LENGTH 32   /* buffers and will misbehave if the buffer used is smaller.   */
-  #elif (RAMSIZE < 4096)       /* So even where it's painfully large portion of ram, we use   */
-    #define BUFFER_LENGTH 32   /* that. We don't enlarge the buffer until we finally get to   */
-  #else                        /* the modern AVRs with >= 4k of RAM. The 128b buffer on the   */
-    #define BUFFER_LENGTH 130  /* official core thus sets a lower bound there, and we add an  */
-    #define BUFFER_NOT_POWER_2 /* extra 2 bytes in order to efficiently write 128b pages to   */
-  #endif                       /* the common AT24-series EEPROM chips at their largest sizes  */
-#endif                         /* where the first two bytes specify the address write at      */
+  #if (RAMSIZE < 256)
+    #define BUFFER_LENGTH 16
+  #elif (RAMSIZE < 4096)
+    #define BUFFER_LENGTH 32
+  #else
+    #define BUFFER_LENGTH 130
+    #define BUFFER_NOT_POWER_2
+  #endif
+#endif
 
 
 #define TWI_TIMEOUT_ENABLE    // Enabled by default, might be disabled for debugging or other reasons
-
 // #define TWI_ERROR_ENABLED
 
 // The error result may not be accurate, it just helps narrowing the problem down
@@ -103,14 +118,12 @@ SOFTWARE.
 #if defined(TWI_ERROR_ENABLED)
   #define TWI_ERROR_VAR   twi_error
   #define TWI_INIT_ERROR  uint8_t TWI_ERROR_VAR = TWI_NO_ERR
-
   #define TWI_CHK_ERROR(x) TWI_ERROR_VAR == x
   #define TWI_SET_ERROR(x) TWI_ERROR_VAR = x
   #define TWI_SAVE_ERROR(x) x = TWI_ERROR_VAR
 #else
   #define TWI_ERROR_VAR     {}
   #define TWI_INIT_ERROR    {}
-
   #define TWI_CHK_ERROR(x)  (true)
   #define TWI_SET_ERROR(x)  {}
   #define TWI_SAVE_ERROR(x) {}
@@ -134,13 +147,10 @@ struct twiDataBools {       // using a struct so the compiler can use skip if bi
 
 struct twiData {
   TWI_t *_module;
-
   struct twiDataBools _bools;      // the structure to hold the bools for the class
-
   #if defined(TWI_ERROR_ENABLED)
     uint8_t _errors;
   #endif
-
   uint8_t _clientAddress;
   #if defined(TWI_MERGE_BUFFERS)
     uint8_t _trHead;
@@ -153,21 +163,17 @@ struct twiData {
   #endif
   #if defined(TWI_MANDS)
     uint8_t _incomingAddress;
-
     uint8_t _trHeadS;
     uint8_t _trTailS;
   #endif
-
   void (*user_onRequest)(void);
   void (*user_onReceive)(int);
-
   #if defined(TWI_MERGE_BUFFERS)
     uint8_t _trBuffer[BUFFER_LENGTH];
   #else
     uint8_t _txBuffer[BUFFER_LENGTH];
     uint8_t _rxBuffer[BUFFER_LENGTH];
   #endif
-
   #if defined(TWI_MANDS)
     uint8_t _trBufferS[BUFFER_LENGTH];
   #endif
@@ -175,19 +181,18 @@ struct twiData {
 };
 
 
-uint8_t  TWI_advancePosition(uint8_t pos);  // returns the next Position with Round-Robin functionality
-
-void     TWI_MasterInit(struct        twiData *_data);
-void     TWI_SlaveInit(struct      twiData *_data, uint8_t address, uint8_t receive_broadcast, uint8_t second_address);
+uint8_t  TWI_advancePosition(uint8_t           pos);  // returns the next Position with Round-Robin functionality
+void     TWI_MasterInit(struct      twiData *_data);
 void     TWI_Flush(struct           twiData *_data);
 void     TWI_Disable(struct         twiData *_data);
-void     TWI_DisableMaster(struct     twiData *_data);
-void     TWI_DisableSlave(struct   twiData *_data);
-void     TWI_MasterSetBaud(struct     twiData *_data, uint32_t frequency);
+void     TWI_DisableMaster(struct   twiData *_data);
+void     TWI_DisableSlave(struct    twiData *_data);
 uint8_t  TWI_Available(struct       twiData *_data);
-uint8_t  TWI_MasterWrite(struct       twiData *_data, bool send_stop);
-uint8_t  TWI_MasterRead(struct        twiData *_data, uint8_t bytesToRead, bool send_stop);
-void     TWI_HandleSlaveIRQ(struct twiData *_data);
-uint8_t  TWI_MasterCalcBaud(uint32_t frequency);  // moved to twi_pins.h due to license incompatibilities
+void     TWI_HandleSlaveIRQ(struct  twiData *_data);
+uint8_t  TWI_MasterWrite(struct     twiData *_data, bool send_stop);
+void     TWI_MasterSetBaud(struct   twiData *_data, uint32_t frequency);
+uint8_t  TWI_MasterRead(struct      twiData *_data, uint8_t bytesToRead, bool send_stop);
+void     TWI_SlaveInit(struct       twiData *_data, uint8_t address, uint8_t receive_broadcast, uint8_t second_address);
+uint8_t  TWI_MasterCalcBaud(uint32_t frequency);
 
 #endif
