174422e6b872859841e6c12029167c1b176eb7fe
Spence Konde <spencekonde@gmail.com>
2022-08-10 01:29:14 -0400

    Overharmonized an  example

--- megaavr/libraries/SoftwareSerial/examples/SoftwareSerialExample/SoftwareSerialExample.ino
+++ megaavr/libraries/SoftwareSerial/examples/SoftwareSerialExample/SoftwareSerialExample.ino
@@ -17,7 +17,7 @@
 */
 #include <SoftwareSerial.h>
 
-SoftwareSerial mySerial(PIN_PD5, PIN_PD4); // RX, TX
+SoftwareSerial mySerial(PIN_PA7, PIN_PA6); // RX, TX
 
 void setup() {
   // Open serial communications and wait for port to open:
