2910eb4283b0e0f0cb1ff38802a9f217d54d02bb
Spence Konde <spencekonde@gmail.com>
2023-04-25 19:00:20 -0400

    fix bugs in MTC library, hopefully make parts.h pass style check

--- megaavr/libraries/megaTinyCore/src/megaTinyCore.h
+++ megaavr/libraries/megaTinyCore/src/megaTinyCore.h
@@ -1,4 +1,6 @@
 #include <Arduino.h>
+#ifndef __MEGATINYCORE_H
+#define __MEGATINYCORE_H
 // in ADCErrors.cpp
 // ADC error interpretation helper functions
 #ifdef __cplusplus
@@ -9,6 +11,53 @@
 #endif
 // Reset immdiately using software reset. The bootloader, if present will run.
 
+extern const uint8_t bit_mask_to_position[];
+extern const uint8_t bit_mask_to_position_n[];
+
+inline uint8_t bitMaskToPosition_by_Math(uint8_t bitmask) {
+  if(__builtin_constant_p(bitmask)) {
+    if (bitmask > 128) {
+      badArg("bitMaskToPosition is being called a constant which is not a bitmask");
+      return 255;
+    }
+  }
+  __asm__ __volatile__ (
+    "eor r0, r0"      "\n\t"
+    "dec r0"          "\n\t" // set temp register to 255
+    "lsr %0"          "\n\t" // Loop comes back to here where we shift bitmask right
+    "inc r0"          "\n\t" // increment the counter so if 1 is first shift we get 0
+    "brcc .-6"        "\n\t" // if it's a 0 rightshift repeat
+    "cpi %0, 0"       "\n\t" // Now we've gotten a 1, is what's left 0?
+    "breq .+4"        "\n\t" // if yes skip next 2 isn
+    "ldi %0, 255"     "\n\t" // if no load 255 into %0
+    "rjmp .+2"        "\n\t" // and skip the last isn.
+    "mov %0, r0"      "\n\t" // otherwise copy r0 into %0
+    : "+d" ((uint8_t) bitmask));
+  return bitmask;
+}
+inline uint8_t bitMaskToPosition_by_LUT(uint8_t bitmask) {
+  if(__builtin_constant_p(bitmask)) {
+    if (bitmask > 128) {
+      badArg("bitMaskToPosition is being called a constant which is not a bitmask");
+      return 255;
+    }
+  } else { //here we need to do a bounds check
+    if (bitmask > 128) {
+      return 255; // Error - called with something not a bit mask!
+    }
+  }
+  return bit_mask_to_position[bitmask];
+}
+inline uint8_t bitMaskToHalfPosition(uint8_t bitmask, bool highnybble) {
+  if (highnybble) {
+    _SWAP(bitmask);
+  }
+  bitmask &= 0x0F;
+  // No further checks table has 15 entries.
+  return bit_mask_to_position_n[bitmask];
+}
+
+
 inline void ResetViaWDT() {
   _PROTECTED_WRITE(WDT.CTRLA, WDT_PERIOD_8CLK_gc); //enable the WDT, minimum timeout
   while (1); // spin until reset
@@ -18,215 +67,8 @@ inline void ResetViaSoftware() {
   _PROTECTED_WRITE(RSTCTRL.SWRR, 1);
 }
 
+uint8_t getTunedOSCCAL(uint8_t osc, uint8_t target);
 
-/* Now we get into the ugly clock stuff */
+void printTuningStatus();
 
-// Display current tuning state in human readable form.
-// *INDENT-OFF*  display this code in human readable form instead of what astyle wants.
-int16_t getTunedOSCCAL(uint8_t osc, uint8_t target) {
-  if (__builtin_constant_p(osc)) {
-    if (osc != 20 && osc !=16) {
-      badArg("The first argument must either 16 or 20, reflecting the nominal internal oscillator speed; it is a constant that is neither of those");
-    }
-    if (__builtin_constant_p(target)) {
-      #if MEGATINYCORE_SERIES == 2
-        if ((target != 16 && target != 20 && target != 24 && target != 25 && target != 30) || (((osc == 20) && (target == 14)) || ((osc == 16) && (target == 32))))
-      #else
-        if ((target != 14 && target != 16 && target != 20 && target != 24 && target != 25) || (((osc == 16) && (target == 30)) || ((osc == 20) && (target == 12))) || target==32)
-      #endif
-      {
-        badArg("The second argument must be a valid unprescaled tuned clock speed. Both arguments are constant, but the latter is not a valid target for the former. See Ref_tuning.md.");
-      }
-    }
-  } else if (__builtin_constant_p(target)) {
-    #if MEGATINYCORE_SERIES == 2
-      if (target != 14 && target != 16 && target != 20 && target != 24 && target != 25 && target != 30 && target != 32)
-    #else
-      if (target != 12 && target != 14 && target != 16 && target != 20 && target != 24 && target != 25 && target != 30)
-    #endif
-    {
-      badArg("The second argument must be a valid unprescaled tuned clock speed");
-    }
-  }
-  uint8_t offset = CLOCK_TUNE_START;
-  #if MEGATINYCORE_SERIES != 2
-    if (osc == 16) {
-      switch (target) {
-        case (25):
-          offset++;
-        /* fall-through */
-        case (24):
-          offset++;
-        /* fall-through */
-        case (20):
-          offset++;
-        /* fall-through */
-        case (16):
-          offset++;
-        /* fall-through */
-        case (14):
-          offset++;
-        /* fall-through */
-        case (12): /* do nothing, but don't run default */
-          break;   /* Hey, it was easy to write, and this is the first time in my life that the switch/case fall-through has made things easier */
-        default:
-          return -2; /* invalid target frequency */
-      }
-    } else if (osc == 20) {
-      offset += 6;
-      switch (target) {
-        case (30):
-          offset++;
-        /* fall-through */
-        case (25):
-          offset++;
-        /* fall-through */
-        case (24):
-          offset++;
-        /* fall-through */
-        case (20):
-          offset++;
-        /* fall-through */
-        case (16):
-          offset++;
-        /* fall-through */
-        case (14): /* do nothing, but don't run default */
-          break;
-        default:
-          return -2; /* invalid target frequency */
-      }
-    } else {
-      return -1; /* invalid center frequency */
-    }
-  #else /* 2-series can go higher */
-    if (osc == 16) {
-      switch (target) {
-        case (30):
-          offset++;
-        /* fall-through */
-        case (25):
-          offset++;
-        /* fall-through */
-        case (24):
-          offset++;
-        /* fall-through */
-        case (20):
-          offset++;
-        /* fall-through */
-        case (16):
-          offset++;
-        /* fall-through */
-        case (14): /* do nothing, but don't run default */
-          break;
-        default:
-          return -2; /* invalid target frequency */
-      }
-    } else if (osc == 20) {
-      offset += 6;
-      switch (target) {
-        case (32):
-          offset++;
-        /* fall-through */
-        case (30):
-          offset++;
-        /* fall-through */
-        case (25):
-          offset++;
-        /* fall-through */
-        case (24):
-          offset++;
-        /* fall-through */
-        case (20):
-          offset++;
-        /* fall-through */
-        case (16): /* do nothing, but don't run default */
-          break;
-        default:
-          return -2; /* invalid target frequency */
-      }
-    } else {
-      return -1; /* invalid center frequency */
-    }
-  #endif
-  uint8_t retval = _SFR_MEM8(offset);
-  if (retval == 255) {
-    return -3; /*chip not tuned*/
-  } else if (retval == 0x80) {
-    return -4; /* Chip tuned and found to not achieve target speed */
-}
-  return retval;
-}
-
-const uint8_t speeds[] = {10, 12, 16, 20, 24, 25, 30, 32};
-
-void printTuningStatus() {
-
-  int16_t tuning1616 = getTunedOSCCAL(16, 16);
-  int16_t tuning2020 = getTunedOSCCAL(20, 20);
-  uint8_t _speedlist_offset = 0;
-  uint8_t blank = 0;
-  if (FUSE.OSCCFG == 2) {
-    _speedlist_offset++;
-  }
-  if (MEGATINYCORE_SERIES == 2) {
-    _speedlist_offset++;
-  }
-  Serial.print("This chip has ");
-  if (tuning2020 == 0xFF) {
-    Serial.print(" NOT ");
-  }
-  Serial.println("been tuned for the 20 MHz oscillator.");
-  Serial.print("This chip has ");
-  if (tuning1616 == 0xFF) {
-    Serial.print(" NOT ");
-  }
-  Serial.println("been tuned for the 16 MHz oscillator.");
-  if (tuning1616 != 0xFF) {
-    Serial.println("16 MHz Tuning");
-    for (uint8_t i = 0; i < 6; i++) {
-      Serial.print(speeds[i + _speedlist_offset]);
-      Serial.print(" MHz: ");
-      int16_t temp = getTunedOSCCAL(16, speeds[i + _speedlist_offset]);
-      if (temp == 0xFF) {
-        Serial.println("Blank");
-        blank++;
-      } else if (temp == 0x80) {
-        if (speeds[i + _speedlist_offset] <= 12) {
-          Serial.println("Too low");
-        } else {
-          Serial.println("Too high");
-        }
-      } else if (temp == 0xC0) {
-        Serial.println("Too unstable");
-      } else {
-        Serial.printHex(temp);
-        Serial.println();
-      }
-    }
-    Serial.println();
-  }
-  if (tuning2020 != 0xFF) {
-    Serial.println("20 MHz Tuning");
-    for (uint8_t i = 1; i < 7; i++) {
-      Serial.print(speeds[i + _speedlist_offset]);
-      Serial.print(" MHz: ");
-      int16_t temp = getTunedOSCCAL(16, speeds[i + _speedlist_offset]);
-      if (temp == 0xFF) {
-        Serial.println("Blank");
-        blank++;
-      } else if (temp == 0x80) {
-        if (speeds[i + _speedlist_offset] <= 12) {
-          Serial.println("Too low");
-        } else {
-          Serial.println("Too high");
-        }
-      } else if (temp == 0xC0) {
-        Serial.println("Too unstable");
-      } else {
-        Serial.printHex(temp);
-        Serial.println();
-      }
-    }
-    Serial.println();
-  }
-}
+#endif
